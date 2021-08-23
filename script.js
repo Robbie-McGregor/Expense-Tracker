4ffb030c0ae082ce38d04ce5631e053a6a940ce3
Robbie <mr.robbie.mcgregor@gmail.com>
2021-08-23 18:02:08 +1200

    Fourth video finished

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const dummyTransactions = [
     { id: 1, text: 'Flower', amount: -20},
     { id: 2, text: 'Salary', amount: 300},
     { id: 3, text: 'Book', amount: -10},
-    { id: 4, text: 'Camera', amount: -150},
+    { id: 4, text: 'Camera', amount: 150},
 ];
 
 let transactions = dummyTransactions
@@ -28,7 +28,29 @@ function addTransactionDOM(transaction) {
         ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>
     `
     list.appendChild(item)
-    
+}
+
+//Update the Balance income and expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount)
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+
+    const income = amounts
+                    .filter(item => item > 0)
+                    .reduce((acc, item) => (acc += item), 0)
+                    .toFixed(2)
+
+    const expenses = (amounts
+                    .filter(item => item < 0)
+                    .reduce((acc, item) => (acc += item), 0)
+                    ) * -1
+                    .toFixed(2)
+
+    balance.innerText = total
+    money_plus.innerText =`$${income}`
+    money_minus.innerText = `-$${expenses}`
+
 
 }
 
@@ -38,6 +60,7 @@ function init() {
     list.innerHTML = '';
 
     transactions.forEach((transaction) => addTransactionDOM(transaction))
+    updateValues()
 }
 
 init()
